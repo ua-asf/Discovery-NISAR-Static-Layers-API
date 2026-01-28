9d51b5f5e938e60c4e827c0e56ce789f459dc232
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-01-28 14:08:40 -0900

    wip: add _get_file_name method, test cases. Ran ruff format

--- src/application.py
+++ src/application.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 import re
 import boto3
 import botocore
@@ -8,8 +9,9 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-GRANULE_PATTERN_STR = r'NISAR_L2_\D{2}_(?P<product_type>\D{4})_\d{3}_(?P<track_id>\d{3})_\D_(?P<frame_id>\d{3})_(?:\d{3}_)?(?P<freq_a>\d{2})(?P<freq_b>\d{2})\D*(?P<start_time>\d{8}T\d{6})'
+GRANULE_PATTERN_STR = r"NISAR_L2_\D{2}_(?P<product_type>\D{4})_\d{3}_(?P<track_id>\d{3})_\D_(?P<frame_id>\d{3})_(?:\d{3}_)?(?P<freq_a>\d{2})(?P<freq_b>\d{2})\D*(?P<start_time>\d{8}T\d{6})"
 GRANULE_PATTERN = re.compile(GRANULE_PATTERN_STR)
+
 
 # Template: https://nisar-services.earthdata.nasa.gov/redirect/NISAR_L2_STATIC/{granule_id}.h5
 @dataclass
@@ -21,24 +23,19 @@ class Granule:
     freq_b: str
     start_time: str
 
+
 def lambda_handler(event, context):
-    print(f'boto3 version: {boto3.__version__}')
-    print(f'botocore version: {botocore.__version__}')
+    print(f"boto3 version: {boto3.__version__}")
+    print(f"botocore version: {botocore.__version__}")
 
-    http_method = event['requestContext']['http']['method']
-    path: str = str(event['requestContext']['http']['path']) # '.../.../{granule_id}.h5'
+    http_method = event["requestContext"]["http"]["method"]
+    path: str = str(
+        event["requestContext"]["http"]["path"]
+    )  # '.../.../{granule_id}.h5'
 
-
-    if http_method == "GET": 
-        granule_id = ""
-        if (path.rfind("/") > -1):
-            granule_id = path[path.rfind("/") +1: len(path)]
-            if not granule_id.endswith('.h5'):
-                return {
-                    'statusCode': 400,
-                    'body': "Invalid file, expected .h5 extension"
-                }
-        granule = _get_granule(granule_id)
+    if http_method == "GET":
+        file_name = _get_file_name(path)
+        granule = _get_granule(file_name)
 
     pass
     # return {
@@ -47,12 +44,29 @@ def lambda_handler(event, context):
     # }
 
 
-def _get_granule(granule_id: str) -> Granule:
+def _get_granule(file_name: str) -> Granule:
     # Grab granule metadata from granule_id via regex
-    result = GRANULE_PATTERN.match(granule_id)
+    result = GRANULE_PATTERN.match(file_name)
+
+    if result is None:
+        raise ValueError(f"Source granule file name {file_name} is not valid")
+
     data = result.groupdict()
 
     if any(v is None for v in data.values()):
-        raise ValueError("Invalid source granule file name")
+        raise ValueError(f"Source granule file name {file_name} is not valid")
 
     return Granule(**data)
+
+
+def _get_file_name(path: str) -> str:
+    # verify no additional path parameters are in request and file has the expected extension
+    p = Path(path)
+
+    if len(p.parts) != 2:
+        raise ValueError('Invalid file name provided. Expected "/{granule_id}.h5"')
+
+    if p.suffix.lower() != ".h5":
+        raise ValueError("Invalid file name provided. Expected .h5 file")
+
+    return p.name
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,56 +1,119 @@
-# Below are examples of how data should be parsed from the granule ids of the 4 L2 products
-from NISARStaticLayersAPI.application import Granule, _get_granule
+import pytest
+from NISARStaticLayersAPI.application import Granule, _get_granule, _get_file_name
 
+# Below are examples of how data should be parsed from the granule ids of the 4 L2 products
 gslc_test_data = {
-    'granule_id': 'NISAR_L2_PR_GSLC_055_132_A_029_4005_SHSH_A_20241021T082112_20241021T082134_T00408_N_P_J_001',
-    'granule': Granule(**{
-    'product_type': 'GSLC',
-    'track_id': '132',
-    'frame_id': '029',
-    'freq_a':  '40',
-    'freq_b': '05',
-    'start_time': '20241021T082112'
-})
+    "file_name": "NISAR_L2_PR_GSLC_055_132_A_029_4005_SHSH_A_20241021T082112_20241021T082134_T00408_N_P_J_001.h5",
+    "granule": Granule(
+        **{
+            "product_type": "GSLC",
+            "track_id": "132",
+            "frame_id": "029",
+            "freq_a": "40",
+            "freq_b": "05",
+            "start_time": "20241021T082112",
+        }
+    ),
 }
 
 
 gcov_test_data = {
-    'granule_id': 'NISAR_L2_PR_GCOV_045_112_D_085_2005_DHDH_M_20240621T233525_20240621T233601_T00408_N_F_J_001',
-    'granule': Granule(**{
-    'product_type': 'GCOV',
-    'track_id': '112',
-    'frame_id': '085',
-    'freq_a':  '20',
-    'freq_b': '05',
-    'start_time': '20240621T233525',
-    })
+    "file_name": "NISAR_L2_PR_GCOV_045_112_D_085_2005_DHDH_M_20240621T233525_20240621T233601_T00408_N_F_J_001.h5",
+    "granule": Granule(
+        **{
+            "product_type": "GCOV",
+            "track_id": "112",
+            "frame_id": "085",
+            "freq_a": "20",
+            "freq_b": "05",
+            "start_time": "20240621T233525",
+        }
+    ),
 }
 
 gunw_test_data = {
-    'granule_id': 'NISAR_L2_PR_GUNW_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00407_N_P_J_001',
-    'granule': Granule(**{
-    'product_type': 'GUNW',
-    'track_id': '002',
-    'frame_id': '123',
-    'freq_a':  '40',
-    'freq_b': '00',
-    'start_time': '20240403T084941',
-    })
+    "file_name": "NISAR_L2_PR_GUNW_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00407_N_P_J_001.h5",
+    "granule": Granule(
+        **{
+            "product_type": "GUNW",
+            "track_id": "002",
+            "frame_id": "123",
+            "freq_a": "40",
+            "freq_b": "00",
+            "start_time": "20240403T084941",
+        }
+    ),
 }
 
 goff_test_data = gunw_test_data = {
-    'granule_id': 'NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001',
-    'granule': Granule(**{'product_type': 'GOFF',
-    'track_id': '002',
-    'frame_id': '123',
-    'freq_a':  '40',
-    'freq_b': '00',
-    'start_time': '20240403T084941',
-    })
+    "file_name": "NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001.h5",
+    "granule": Granule(
+        **{
+            "product_type": "GOFF",
+            "track_id": "002",
+            "frame_id": "123",
+            "freq_a": "40",
+            "freq_b": "00",
+            "start_time": "20240403T084941",
+        }
+    ),
 }
 
 test_data = [gslc_test_data, gcov_test_data, gunw_test_data, goff_test_data]
 
+test_granule_paths = [
+    {
+        "path": "/NISAR_L2_PR_GSLC_055_132_A_029_4005_SHSH_A_20241021T082112_20241021T082134_T00408_N_P_J_001.h5",
+        "file_name": "NISAR_L2_PR_GSLC_055_132_A_029_4005_SHSH_A_20241021T082112_20241021T082134_T00408_N_P_J_001.h5",
+    },
+    {
+        "path": "/NISAR_L2_PR_GCOV_045_112_D_085_2005_DHDH_M_20240621T233525_20240621T233601_T00408_N_F_J_001.h5",
+        "file_name": "NISAR_L2_PR_GCOV_045_112_D_085_2005_DHDH_M_20240621T233525_20240621T233601_T00408_N_F_J_001.h5",
+    },
+    {
+        "path": "/NISAR_L2_PR_GUNW_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00407_N_P_J_001.h5",
+        "file_name": "NISAR_L2_PR_GUNW_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00407_N_P_J_001.h5",
+    },
+    {
+        "path": "/NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001.h5",
+        "file_name": "NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001.h5",
+    },
+    {
+        "path": "/hidden/path/NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001.h5",
+        "raises": True,
+    },
+    {
+        "path": "/hidden/path/NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001",
+        "raises": True,
+    },
+    {
+        "path": "/NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001",
+        "raises": True,
+    },
+    {
+        "path": "NISAR_L2_PR_GOFF_039_002_D_123_040_4000_SH_20240403T084941_20240403T084954_20240415T084941_20240415T084954_T00408_N_P_J_001",
+        "raises": True,
+    },
+]
+
+
 def test_get_granule():
     for data in test_data:
-        assert data['granule'] == _get_granule(data['granule_id'])
+        assert data["granule"] == _get_granule(data["file_name"])
+
+    # No match test
+    with pytest.raises(ValueError):
+        _get_granule("This_Should_Raise")
+
+    # Partial match test
+    with pytest.raises(ValueError):
+        _get_granule("NISAR_L2_PR_GOFF_039.h5")
+
+
+def test_get_granule_name():
+    for granule in test_granule_paths:
+        if granule.get("raises"):
+            with pytest.raises(ValueError):
+                _get_file_name(granule["path"])
+        else:
+            assert _get_file_name(granule["path"]) == granule["file_name"]
